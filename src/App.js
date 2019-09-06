3e23d38f9666c32153e2f8dd440bc7a076c48f2c
JosephRojas <josephrojas21@hotmail.com>
2019-09-05 21:10:30 -0500

    apply redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
+import CreateProject from './components/projects/CreateProject'
 
 
 function App() {
@@ -15,8 +16,9 @@ function App() {
       <Switch>
         <Route exact path="/" component={Dashboard} />
         <Route path="/project/:id" component={ProjectDetails}/>
-        <Route path="/sigin" component={SignIn}/>
-        <Route path="/sigup" component={SignUp}/>
+        <Route path="/signin" component={SignIn}/>
+        <Route path="/signup" component={SignUp}/>
+        <Route path="/create" component={CreateProject}/>
 
       </Switch>
     </div>
